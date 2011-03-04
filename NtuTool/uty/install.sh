dd22c529e92b73772b419c892fba7b3e0acbd968
paolocms <>
2011-03-04 18:34:18 +0000

    ntuple handling tool

--- NtuTool/uty/install.sh
+++ NtuTool/uty/install.sh
@@ -10,6 +10,8 @@ sed s#INSTALLDIR#${INSTALLDIR}# uty/envset.csh > envset.tmp
 rm -f uty/envset.csh
 mv envset.tmp uty/envset.csh
 
+source uty/envset.sh
+
 mv Read/test/treeAnalyze.cc Read/src
 rmdir Read/test
 
@@ -18,9 +20,9 @@ rm -rf */CVS
 rm -rf */*/CVS
 rm -rf */*/*/CVS
 
-
 mkdir include
 mkdir lib
 
 cp */interface/* include
+ln -s ${NTU_TOOL_DIR} ${NTU_TOOL_DIR}/include/NtuTool
 
