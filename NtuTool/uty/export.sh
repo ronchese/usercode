02f9d516a031fe61b919d5b86567045eb7494979
paolocms <>
2011-04-29 11:52:12 +0000

    script updated

--- NtuTool/uty/export.sh
+++ NtuTool/uty/export.sh
@@ -5,6 +5,6 @@ tar -czf NtuTool.tgz                                   \
          NtuTool/Common/interface NtuTool/Common/src   \
          NtuTool/Read/interface NtuTool/Read/src       \
          NtuTool/Read/test/treeAnalyze.cc              \
-         NtuTool/uty/install.sh NtuTool/uty/envset.*h  \
+         NtuTool/uty/install.sh NtuTool/uty/envset*h   \
          NtuTool/uty/compile.sh                        \
          NtuTool/uty/go_compileNtuTool.*h NtuTool/uty/compileCMSSW.*h
