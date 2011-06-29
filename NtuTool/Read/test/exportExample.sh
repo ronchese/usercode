67421189d97f28bbee9a62e213f0af657256c1ea
paolocms <>
2011-06-29 13:02:25 +0000

    ntuple handling tool

--- NtuTool/Read/test/exportExample.sh
+++ NtuTool/Read/test/exportExample.sh
@@ -14,6 +14,8 @@ cp ${CMSSW_BASE}/src/NtuTool/Read/test/stubs/EDMSimpleAnalyzer.h   .
 cp ${CMSSW_BASE}/src/NtuTool/Read/test/stubs/EDMSimpleAnalyzer.cc  .
 cp ${CMSSW_BASE}/src/NtuTool/Read/test/ntuList                     .
 cp ${CMSSW_BASE}/src/NtuTool/Read/test/edmList                     .
+cp ${CMSSW_BASE}/src/NtuTool/EDM/test/ntuple_base.root             .
+cp ${CMSSW_BASE}/src/NtuTool/EDM/test/ntuple_edm.root              .
 
 sed -i "s#NtuTool/Common/test/stubs/##" SimpleNtuple.cc
 sed -i "s#NtuTool/Common/test/stubs/##" SimpleAnalyzer.h
