4428a23e50d0db7d0c50a7d8cde848ba07dc1dbb
paolocms <>
2011-12-15 12:20:41 +0000

    include path fixed

--- NtuTool/uty/compile.sh
+++ NtuTool/uty/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd $NTU_TOOL_DIR
+cd ${NTU_TOOL_DIR}
 
 rm -f lib/libNtupleTool.so
-c++ -I.. `root-config --cflags` -fPIC \
+c++ -I ${NTU_TOOL_DIR}/include `root-config --cflags` -fPIC \
     --shared -o $NTU_TOOL_DIR/lib/libNtupleTool.so \
     Common/src/*.cc  Read/src/*.cc
