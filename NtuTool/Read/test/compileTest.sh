0d2208375d22a43a00411287d072a230ea56ae3a
paolocms <>
2012-10-07 23:33:47 +0000

    explicit deletion of old executables added

--- NtuTool/Read/test/compileTest.sh
+++ NtuTool/Read/test/compileTest.sh
@@ -1,9 +1,11 @@
+rm -f treeAnalyze_Base
 c++ `root-config --cflags --glibs` \
     -I ${CMSSW_BASE}/src -o treeAnalyze_Base treeAnalyze.cc \
     ${CMSSW_BASE}/src/NtuTool/Common/test/stubs/SimpleNtuple.cc \
     stubs/SimpleAnalyzer.cc \
     -L ${CMSSW_BASE}/lib/${SCRAM_ARCH} \
     -lNtuToolRead -lNtuToolCommon
+rm -f treeAnalyze_EDM
 c++ `root-config --cflags --glibs` \
     -I ${CMSSW_BASE}/src -o treeAnalyze_EDM treeAnalyze.cc \
     ${CMSSW_BASE}/src/NtuTool/Common/test/stubs/SimpleNtuple.cc \
