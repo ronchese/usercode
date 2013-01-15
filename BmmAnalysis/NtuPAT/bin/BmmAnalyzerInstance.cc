85b850b4923001e10e7381667c071144c168dbc7
paolocms <>
2013-01-15 15:24:11 +0000

    This commit was manufactured by cvs2svn to create tag 'V01-02-07'.
    Sprout from master 2013-01-15 15:24:10 UTC paolocms 'null file handling added'
    Delete:
        BmmAnalysis/NtuPAT/BuildFile.xml
        BmmAnalysis/NtuPAT/bin/BmmAnalyzer.cc
        BmmAnalysis/NtuPAT/bin/BmmAnalyzer.h
        BmmAnalysis/NtuPAT/bin/BmmAnalyzerInstance.cc
        BmmAnalysis/NtuPAT/bin/BmmLightNtuple.cc
        BmmAnalysis/NtuPAT/bin/BmmLightNtuple.h
        BmmAnalysis/NtuPAT/bin/BmmMacro.C
        BmmAnalysis/NtuPAT/bin/BuildFile.xml
        BmmAnalysis/NtuPAT/bin/TreeLightReader.h
        BmmAnalysis/NtuPAT/bin/bmm.cc
        BmmAnalysis/NtuPAT/bin/cfg_fwfull.py
        BmmAnalysis/NtuPAT/bin/cfg_fwlite.py
        BmmAnalysis/NtuPAT/bin/compileBmmAnalyzer_FW.csh
        BmmAnalysis/NtuPAT/bin/ntu_dummy.list
        BmmAnalysis/NtuPAT/bin/setupMacroAnalysis.csh
        BmmAnalysis/NtuPAT/interface/BmmAnalyzerFW.h
        BmmAnalysis/NtuPAT/interface/BmmNtuple.h
        BmmAnalysis/NtuPAT/interface/BmmNtupleBranch.h
        BmmAnalysis/NtuPAT/interface/BmmNtupleBranch.icc
        BmmAnalysis/NtuPAT/interface/BmmNtupleData.h
        BmmAnalysis/NtuPAT/interface/BmmPATToNtuple.h
        BmmAnalysis/NtuPAT/plugins/BmmPATAnalyzer.cc
        BmmAnalysis/NtuPAT/plugins/BmmPATAnalyzer.h
        BmmAnalysis/NtuPAT/plugins/BuildFile.xml
        BmmAnalysis/NtuPAT/src/BmmAnalyzerFW.cc
        BmmAnalysis/NtuPAT/src/BmmNtuple.cc
        BmmAnalysis/NtuPAT/src/BmmNtupleData.cc
        BmmAnalysis/NtuPAT/src/BmmPATToNtuple.cc
        HTTAnalysis/NtuPAT/BuildFile.xml
        HTTAnalysis/NtuPAT/bin/BuildFile.xml
        HTTAnalysis/NtuPAT/bin/HTTAnalyzer.cc
        HTTAnalysis/NtuPAT/bin/HTTAnalyzer.h
        HTTAnalysis/NtuPAT/bin/HTTAnalyzerInstance.cc
        HTTAnalysis/NtuPAT/bin/HTTLightNtuple.cc
        HTTAnalysis/NtuPAT/bin/HTTLightNtuple.h
        HTTAnalysis/NtuPAT/bin/HTTMacro.C
        HTTAnalysis/NtuPAT/bin/TreeLightReader.h
        HTTAnalysis/NtuPAT/bin/cfg_fwfull.py
        HTTAnalysis/NtuPAT/bin/cfg_fwlite.py
        HTTAnalysis/NtuPAT/bin/compileHTTAnalyzer_FW.csh
        HTTAnalysis/NtuPAT/bin/hTT.cc
        HTTAnalysis/NtuPAT/bin/ntu_dummy.list
        HTTAnalysis/NtuPAT/bin/setupMacroAnalysis.csh
        HTTAnalysis/NtuPAT/interface/HTTAnalyzerFW.h
        HTTAnalysis/NtuPAT/interface/HTTNtuple.h
        HTTAnalysis/NtuPAT/interface/HTTNtupleBranch.h
        HTTAnalysis/NtuPAT/interface/HTTNtupleBranch.icc
        HTTAnalysis/NtuPAT/interface/HTTNtupleData.h
        HTTAnalysis/NtuPAT/interface/HTTPATToNtuple.h
        HTTAnalysis/NtuPAT/plugins/BuildFile.xml
        HTTAnalysis/NtuPAT/plugins/HTTPATAnalyzer.cc
        HTTAnalysis/NtuPAT/plugins/HTTPATAnalyzer.h
        HTTAnalysis/NtuPAT/src/HTTAnalyzerFW.cc
        HTTAnalysis/NtuPAT/src/HTTNtuple.cc
        HTTAnalysis/NtuPAT/src/HTTNtupleData.cc
        HTTAnalysis/NtuPAT/src/HTTPATToNtuple.cc

--- BmmAnalysis/NtuPAT/bin/BmmAnalyzerInstance.cc
+++
@@ -1,8 +0,0 @@
-#include "BmmAnalyzer.h"
-#include "NtuTool/Read/interface/TreeReader.h"
-
-class BmmAnalyzerInstance: public BmmAnalyzer, public TreeReader {
-};
-
-static BmmAnalyzerInstance ai;
-
--- BmmAnalysis/NtuPAT/bin/setupMacroAnalysis.csh
+++
@@ -1,6 +0,0 @@
-
-#### script to setup symbolic links needed to compile BmmMacro.C
-
-cd `dirname $0`
-ln -sf ../../../BmmAnalysis
-ln -sf ../../../NtuTool
