3658a9748ac6400a0be244e09ed5bcf8a446dccc
paolocms <>
2013-02-01 14:29:50 +0000

    several improvements

---
+++ BmmAnalysis/NtuPAT/bin/BmmAnalyzerUtil.h
@@ -0,0 +1,80 @@
+#ifndef BmmAnalyzerUtil_H
+#define BmmAnalyzerUtil_H
+
+#define FULL 2
+#define BARE 3
+
+namespace edm {
+  class ParameterSet;
+  class Event;
+  class EventBase;
+  class EventSetup;
+}
+
+#include "BmmAnalysis/NtuPAT/interface/BmmEventSelect.h"
+
+#  if UTIL_USE == FULL
+
+#include "BmmAnalysis/NtuPAT/interface/BmmNtuple.h"
+class BmmAnalyzerUtil: public BmmNtuple,
+                       public BmmEventSelect {
+
+#elif UTIL_USE == BARE
+
+// get the bare ntuple definition, with no usage of other utilities
+// in addition to Bmm specific ones
+// "TreeLightReader" simply forward calls directly to ROOT with no 
+// additional operation
+#include "BmmLightNtuple.h"
+class BmmAnalyzerUtil: public BmmLightNtuple,
+                       public BmmEventSelect {
+
+#else
+#error use -D UTIL_USE=FULL or -D UTIL_USE=BARE
+#endif
+
+ public:
+
+  BmmAnalyzerUtil();
+  virtual ~BmmAnalyzerUtil();
+
+ protected:
+
+  const edm::ParameterSet* parameterSet ;
+  const edm::EventBase   * currentEvBase;
+  const edm::Event       * currentEvent;
+  const edm::EventSetup  * currentEvSetup;
+
+  virtual void beginJob();
+
+  virtual void getEntry( int ientry );
+  virtual void getEntry( TBranch* branch, int ientry );
+
+  bool goodJet( unsigned int iJet );
+
+ private:
+
+  // cuts to select good jets
+  int    jetNDaumin;
+  int    jetNDaumax;
+  double jetNHFmin;
+  double jetNHFmax;
+  double jetNEFmin;
+  double jetNEFmax;
+  double jetCHFmin;
+  double jetCHFmax;
+  double jetCEFmin;
+  double jetCEFmax;
+  double jetNCHmin;
+  double jetNCHmax;
+  double jetEtaCut;
+
+  // dummy copy and assignment constructors
+  BmmAnalyzerUtil( const BmmAnalyzerUtil& );
+  BmmAnalyzerUtil& operator=( const BmmAnalyzerUtil& );
+
+};
+
+
+#endif
+
